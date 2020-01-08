f8ac67b6eae35808402967f011cfeae6ea290046
JoshuaTroyBlake <joshua.tibbs@students.plymouth.ac.uk>
2020-01-08 22:36:10 +0000

    Folder rename

--- startbootstrap-freelancer-gh-pages/showLights.js
+++
@@ -1,8 +0,0 @@
-function showLights1() {
-    var x = document.createElement("IMG");
-    x.setAttribute("src", "poor.png");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
-    document.body.appendChild(x);
-}
